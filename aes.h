60e8b030de2ab16c2af174b24cd065dc1d01cb6a
Jonathan Plata <jonathan.plata@lightshipsec.com>
2023-01-18 13:18:44 -0500

    list aes-ofb support

--- aes.h
+++ aes.h
@@ -8,6 +8,7 @@ int ACVP_TEST_vs_aes_cfb1(cJSON *j, void *options, cJSON *out);
 int ACVP_TEST_vs_aes_cfb8(cJSON *j, void *options, cJSON *out);
 int ACVP_TEST_vs_aes_cfb128(cJSON *j, void *options, cJSON *out);
 int ACVP_TEST_vs_aes_gcm(cJSON *j, void *options, cJSON *out);
-int ACVP_TEST_vs_aes_kw(cJSON *j, void *options, cJSON *out);
+int ACVP_TEST_vs_aes_ofb(cJSON *j, void *options, cJSON *out);
+//int ACVP_TEST_vs_aes_kw(cJSON *j, void *options, cJSON *out);
 
 #endif
